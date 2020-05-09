7fbe84834131573cb3da8efb394ea1c2597694b3
Paul Freakn Baker <paul.nelson.baker@gmail.com>
2020-05-08 21:39:56 -0600

    Huh. Can iterate and programatically add text after the codeblock. That's kinda cool

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
 use std::io::{Read, stdin, stdout};
 use std::path::{Path, PathBuf};
 use std::fs::create_dir_all;
+
+use log::{info, trace, warn};
+use clap::{App, Arg, ArgMatches, SubCommand};
+
 use mdbook::book::{Book, BookItem};
 use mdbook::errors::Error;
 use mdbook::preprocess::{CmdPreprocessor, Preprocessor, PreprocessorContext};
-use clap::{App, Arg, ArgMatches, SubCommand};
+use log::LevelFilter;
+
+use pulldown_cmark::{CowStr, Parser, Event, Tag, CodeBlockKind, LinkType};
+use pulldown_cmark_to_cmark::cmark;
+use markedit::{Matcher, Rewriter};
+
 
 fn main() -> Result<(), Error> {
+    let preprocessor = plantuml_renderer_preprocessor::default();
     let matches = get_clap().get_matches();
-    if let Some(_sub_args) = matches.subcommand_matches("supports") {
+    if let Some(support_subcommand) = matches.subcommand_matches("supports") {
+        // if let Some(renderer_argument) = support_subcommand.args.get("renderer") {
+        //     // preprocessor.supports_renderer(renderer_argument as String);
+        // }
         return Ok(());
     }
 
-    let std_in = stdin();
-    let (ctx, book) = CmdPreprocessor::parse_input(std_in)?;
-    let nop_preprocessor = Nop::default();
-    let resulting_book = nop_preprocessor.run(&ctx, book)?;
+    let (context, book) = CmdPreprocessor::parse_input(stdin())?;
+
+    // let plantuml_build_directory = determine_plantuml_output_directory(&context);
+    // create_dir_all(plantuml_build_directory)?;
+    let log_file = determine_log_file(&context);
+    create_dir_all(&log_file.parent().unwrap())?;
+    simple_logging::log_to_file(log_file, LevelFilter::Info)?;
+    info!("Logging initiated");
+
+    let resulting_book = preprocessor.run(&context, book)?;
     serde_json::to_writer(stdout(), &resulting_book)?;
     Ok(())
 }
@@ -27,11 +46,6 @@ fn get_clap() -> App<'static, 'static> {
         .version(VERSION)
         .author("Paul Freakn Baker")
         .about("A preprocessor that will replace some inline codeblocks with rendered PlantUML")
-        // .arg(
-        //     Arg::with_name("log")
-        //         .short("l")
-        //         .help("Log to './output.log' (may help troubleshooting rendering issues)."),
-        // )
         .subcommand(
             SubCommand::with_name("supports")
                 .arg(Arg::with_name("renderer").required(true))
@@ -40,20 +54,35 @@ fn get_clap() -> App<'static, 'static> {
 }
 
 #[derive(Default)]
-pub struct Nop;
+struct plantuml_renderer_preprocessor {
+}
 
-impl Preprocessor for Nop {
+impl Preprocessor for plantuml_renderer_preprocessor {
     fn name(&self) -> &str {
         "plantuml-renderer"
     }
 
     fn run(&self, context: &PreprocessorContext, mut book: Book) -> Result<Book, Error> {
-        let plantuml_build_directory = determine_plantuml_output_directory(context);
-        create_dir_all(plantuml_build_directory)?;
+        let _plantuml_build_directory = determine_plantuml_output_directory(context);
 
         book.for_each_mut(|current_item: &mut BookItem| {
             if let BookItem::Chapter(ref mut current_chapter) = *current_item {
-                current_chapter.content = "Lol chapter modified".to_owned()
+                info!("Working Chapter: {}", current_chapter.name);
+
+                let events_iterator = markedit::parse(&current_chapter.content);
+
+                let matcher = plantuml_codeblock_matcher::default().falling_edge();
+                let rewriter = markedit::insert_markdown_before("(look, plantuml lol)", matcher);
+
+                let mutated_events: Vec<_> = markedit::rewrite(events_iterator, rewriter).collect();
+
+                let mut content_buffer = String::with_capacity(current_chapter.content.len());
+                current_chapter.content = cmark(mutated_events.iter(), &mut content_buffer, None)
+                    .map(|_| content_buffer)
+                    .map_err(|err| {
+                        Error::from(format!("Markdown serialization failed: {}", err))
+                    })
+                    .unwrap();
             }
         });
         Ok(book)
@@ -64,13 +93,61 @@ impl Preprocessor for Nop {
     }
 }
 
-/// Takes the context root of the book and concatinates the build directory
-/// and then appends the plantuml directory to that. This works because the build
-/// directory is given to us relative to the 
+#[derive(Default)]
+struct plantuml_codeblock_matcher {
+    is_plantuml: bool,
+}
+
+impl Matcher for plantuml_codeblock_matcher {
+    fn matches_event(&mut self, event: &pulldown_cmark::Event<'_>) -> bool {
+        match event {
+            Event::Start(Tag::CodeBlock(CodeBlockKind::Fenced(codeblock_language))) if self.is_plantuml(codeblock_language) => {
+                info!("Found plantuml start");
+                self.is_plantuml = true;
+            },
+            Event::End(Tag::CodeBlock(CodeBlockKind::Fenced(codeblock_language))) if self.is_plantuml(codeblock_language) => {
+                info!("Found plantuml end");
+                self.is_plantuml = false;
+                return true
+            }
+            _ => {},
+        }
+        self.is_plantuml
+    }
+}
+
+impl plantuml_codeblock_matcher {
+    fn is_plantuml(&self, value: &pulldown_cmark::CowStr<>) -> bool{
+        value.to_string() == "plantuml".to_string()
+    }
+}
+
+// struct plantuml_codeblock_render_rewriter {
+
+// }
+
+// impl Rewriter for plantuml_codeblock_render_rewriter {
+
+// }
+
+/// Takes the context root of the book and concatinates the build directory.
+/// This works because the build directory is given to us relative to the
+/// project root
+fn determine_build_directory(context: &PreprocessorContext) -> PathBuf {
+    let mut build_directory = PathBuf::new();
+    build_directory.push(&context.root);
+    build_directory.push(&context.config.build.build_dir);
+    build_directory
+}
+
 fn determine_plantuml_output_directory(context: &PreprocessorContext) -> PathBuf {
-    let mut plantuml_directory = PathBuf::new();
-    plantuml_directory.push(&context.root);
-    plantuml_directory.push(&context.config.build.build_dir);
+    let mut plantuml_directory = determine_build_directory(context);
     plantuml_directory.push("plantuml");
     plantuml_directory
+}
+
+fn determine_log_file(context: &PreprocessorContext) -> PathBuf {
+    let mut log_file = determine_plantuml_output_directory(context);
+    log_file.push("plantuml-renderer.log");
+    log_file
 }
